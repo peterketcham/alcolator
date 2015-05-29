d6bba6dbb13a5d674784b72ee0cd66df35091e53
Peter Ketcham <peter.ketcham@gmail.com>
2015-05-29 14:42:14 -0500

    Initial Commit

---
+++ Alcolator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alcolator
+//
+//  Created by Peter Ketcham on 5/29/15.
+//  Copyright (c) 2015 Peter Ketcham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Alcolator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Alcolator
+//
+//  Created by Peter Ketcham on 5/29/15.
+//  Copyright (c) 2015 Peter Ketcham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
